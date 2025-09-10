4283ac95e5798a92d7f9c8e404eb3e9198c34911
Vladislav <vladjigalo07@gmail.com>
2025-09-10 21:49:04 +0300

    exersice ready !

--- app/main.py
+++ app/main.py
@@ -5,18 +5,41 @@ def format_linter_error(error: dict) -> dict:
         "message": error.get("text"),
         "name": error.get("code"),
         "source": "flake8",
-        }
+    }
+
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
         "path": file_path,
-        "errors": [{"line": e["line_number"], "column": e["column_number"], "message": e["text"], "name": e["code"], "source": "flake8"} for e in errors],
-        "status": "passed" if not errors else "failed"
+        "errors": [
+            {
+                "line": e["line_number"],
+                "column": e["column_number"],
+                "message": e["text"],
+                "name": e["code"],
+                "source": "flake8",
+            }
+            for e in errors
+        ],
+        "status": "passed" if not errors else "failed",
     }
 
 
-
 def format_linter_report(linter_report: dict) -> list:
-    return [{"path": f, "errors": [{"line": e["line_number"], "column": e["column_number"], "message": e["text"], "name": e["code"], "source": "flake8"} for e in errs], "status": "passed" if not errs else "failed"} for f, errs in linter_report.items()]
-
-
+    return [
+        {
+            "path": f,
+            "errors": [
+                {
+                    "line": e["line_number"],
+                    "column": e["column_number"],
+                    "message": e["text"],
+                    "name": e["code"],
+                    "source": "flake8",
+                }
+                for e in errs
+            ],
+            "status": "passed" if not errs else "failed",
+        }
+        for f, errs in linter_report.items()
+    ]
